182d59484ddca0cd012167612652714fd44434d0
adi-uchiha <adi.cs.night@gmail.com>
2024-03-30 19:31:08 +0530

    Faculty not centering

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
+import Faculty from "@sections/faculty/Faculty";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="absolute h-fit w-full">
+    <>
+    <div className="h-fit w-full">
 
       <div className="relative h-[100vh] w-full bg-[url('/assets/bg_cover.png')] bg-cover bg-top grid place-items-center">
         <div className="h-full w-full bg-black bg-opacity-60 absolute"></div>
-        <div className="flex flex-col items-center gap-8   w-[70%] z-10 capitalize">
+        <div className="flex flex-col items-center gap-8 w-[70%] z-10 capitalize">
           <p className="text-center font-semibold text-5xl text-[#08fcff] pb-8">PVG’S COLLEGE OF ENGINEERING AND TECHNOLOGY & G. K. PATE (WANI) INSTITUTE OF MANAGEMENT</p>
           <p className="text-center text-4xl font">Artificial Intelligence and Data Science Engineering Association of Students </p>
           <p className="text-center text-3xl text-[#08fcff]">(AIDEAS)</p>
@@ -15,5 +17,8 @@ export default function Home() {
       </div>
 
     </div>
+
+    <Faculty />
+    </>
   );
 }
---
+++ sections/faculty/Faculty.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Faculty = () => {
+  return (
+    <div className=''>
+
+    <div className='text-center font-semibold text-5xl text-[#08fcff] pb-8 underline'>FACULTY</div>
+
+    <div className="box-container flex justify-center items-center">  {/* Added a new container class */}
+        <div className="box">  {/* Changed class name for clarity */}
+          BOXX
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Faculty
